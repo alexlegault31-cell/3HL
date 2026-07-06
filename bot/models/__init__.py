015ec657f87641fb603817955adcf5b949d45f8a
alexlegault31-cell <alexlegault31@gmail.com>
2026-07-06 02:43:22 -0400

    Create __init__.py

---
+++ bot/models/__init__.py
@@ -0,0 +1,41 @@
+################################################################
+FILE PATH TO TYPE ON GITHUB: bot/models/__init__.py
+################################################################
+from bot.models.base import Base  # noqa: F401
+from bot.models.user import User  # noqa: F401
+from bot.models.season import Season  # noqa: F401
+from bot.models.team import Team, TeamSeason  # noqa: F401
+from bot.models.player import Player, PlayerSeason, PlayerTeamLink  # noqa: F401
+from bot.models.schedule import ScheduleGame  # noqa: F401
+from bot.models.game import Game, GameImport  # noqa: F401
+from bot.models.stats import PlayerGameStat, GoalieGameStat, TeamGameStat  # noqa: F401
+from bot.models.standings import StandingsEntry  # noqa: F401
+from bot.models.forfeit import Forfeit  # noqa: F401
+from bot.models.award import Award, AwardWinner  # noqa: F401
+from bot.models.transaction import Transaction  # noqa: F401
+from bot.models.settings import GuildSetting  # noqa: F401
+
+__all__ = [
+    "Base",
+    "User",
+    "Season",
+    "Team",
+    "TeamSeason",
+    "Player",
+    "PlayerSeason",
+    "PlayerTeamLink",
+    "ScheduleGame",
+    "Game",
+    "GameImport",
+    "PlayerGameStat",
+    "GoalieGameStat",
+    "TeamGameStat",
+    "StandingsEntry",
+    "Forfeit",
+    "Award",
+    "AwardWinner",
+    "Transaction",
+    "GuildSetting",
+]
+
+===== END OF FILE, COPY UP TO HERE =====
